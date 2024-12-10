443b2c19cc2b8e7a73a829c8792411f852608dd1
packcb <packcb2@naver.com>
2024-12-10 21:23:38 +0900

    최신화

--- ServerCommon/PacketDefine.cs
+++ ServerCommon/PacketDefine.cs
@@ -3,24 +3,17 @@
     public enum ERROR_CODE : short
     {
         NONE = 0, // 에러가 아니다
-
-        // 서버 초기화 에라
-        REDIS_INIT_FAIL = 1,    // Redis 초기화 에러
-
         // 로그인 
         ADD_USER_DUPLICATE_SESSION = 1001, 
         ADD_USER_DUPLICATE_USERID = 1002,
         REMOVE_USER_SEARCH_FAILURE_USER_ID = 1003,
-        USER_AUTH_SEARCH_FAILURE_USER_ID = 1004,
-        USER_AUTH_ALREADY_SET_AUTH = 1005,
-        LOGIN_ALREADY_WORKING = 1006,
-        LOGIN_FULL_USER_COUNT = 1007,
+        LOGIN_ALREADY_WORKING = 1004,
+        LOGIN_FULL_USER_COUNT = 1005,
 
-        ROOM_ENTER_INVALID_STATE = 1021,
-        ROOM_ENTER_INVALID_USER = 1022,
-        ROOM_ENTER_ERROR_SYSTEM = 1023,
-        ROOM_ENTER_INVALID_ROOM_NUMBER = 1024,
-        ROOM_ENTER_FAIL_ADD_USER = 1025,
+        ROOM_ENTER_INVALID_STATE = 1006,
+        ROOM_ENTER_INVALID_USER = 1007,
+        ROOM_ENTER_INVALID_ROOM_NUMBER = 1008,
+        ROOM_ENTER_FAIL_ADD_USER = 1009,
     }
 
     public enum PACKETID : int
@@ -30,19 +23,17 @@
 
         REQ_LOGIN = 1002,
         RES_LOGIN = 1003,
-        NTF_MUST_CLOSE = 1005,
+        NTF_MUST_CLOSE = 1004,
+        REQ_ROOM_ENTER = 1005,
+        RES_ROOM_ENTER = 1006,
+        NTF_ROOM_USER_LIST = 1007,
+        NTF_ROOM_NEW_USER = 1008,
+        REQ_ROOM_LEAVE = 1009,
+        RES_ROOM_LEAVE = 1010,
+        NTF_ROOM_LEAVE_USER = 1011,
 
-        REQ_ROOM_ENTER = 1015,
-        RES_ROOM_ENTER = 1016,
-        NTF_ROOM_USER_LIST = 1017,
-        NTF_ROOM_NEW_USER = 1018,
-
-        REQ_ROOM_LEAVE = 1021,
-        RES_ROOM_LEAVE = 1022,
-        NTF_ROOM_LEAVE_USER = 1023,
-
-        REQ_ROOM_CHAT = 1026,
-        NTF_ROOM_CHAT = 1027,
+        REQ_ROOM_CHAT = 1012,
+        NTF_ROOM_CHAT = 1013,
 
         CS_END = 1100,
 
@@ -52,14 +43,9 @@
 
         NTF_IN_CONNECT_CLIENT = 8011,
         NTF_IN_DISCONNECT_CLIENT = 8012,
+        NTF_IN_ROOM_LEAVE = 8013,
 
-        REQ_SS_SERVERINFO = 8021,
-        RES_SS_SERVERINFO = 8023,
-
-        REQ_IN_ROOM_ENTER = 8031,
-        RES_IN_ROOM_ENTER = 8032,
-
-        NTF_IN_ROOM_LEAVE = 8036,
+        SS_END = 8100,
     }
 
 
